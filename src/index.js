81e5f157197f77a1edc540b00f70923df68d2d67
Kate Bolshakova <katassr@mail.ru>
2023-02-13 13:25:39 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@
 
 module.exports = function towelSort (matrix) {
   let arr = []
-  let str = matrix.length
+  if ( matrix === undefined  ) {return arr}
+  if (matrix.length === 0) { return arr}
+  let str = matrix.length 
   for (let i = 0; i < str; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
         let column = i % 2 === 0 
         ? j
         : (matrix[i].length - j - 1)
           arr.push(matrix[i][column])
-  return arr;
 }
-  }
+  } return arr
 }
